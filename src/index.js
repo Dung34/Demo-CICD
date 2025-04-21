2e3e728c1a1f75b9fc9d7caa349370e564674a08
Dung34 <2k3dungx@gmail.com>
2025-04-21 17:00:42 +0700

    add .env

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 const cors = require("cors");
 app.use(cors());
